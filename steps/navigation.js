ba2a778603f911215091865445273935929b24be
Chris Harbert <chris.harbert@testery.io>
2020-10-01 14:48:18 -0500

    Adjusting click button.

--- steps/navigation.js
+++ steps/navigation.js
@@ -5,7 +5,9 @@ When("I navigate to the webapp", function() {
   var testUrl = process.env.TEST_URL === undefined ? "localhost:3000" : process.env.TEST_URL;
   browser.url(testUrl);
   if (browser.getTitle() == "Tunnel website ahead!") {
+    console.log("Viewing tunnel site. Clicking next button.");
     $(".btn-primary").click();
+    browser.pause(1000);
   } else {
     console.log("Browser title is: " + browser.getTitle());
   }
